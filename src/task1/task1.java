db3091300aef8b4896608d7afdce8f773ff2bcae
Sandu Bujor <alexandru.bujor@isa.utm.md>
2025-10-08 18:22:21 +0300

    Finalization and final construction for the task1 to work as excpected.

---
+++ src/task1/task1.java
@@ -0,0 +1,46 @@
+package task1;
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introdu cheia pentru Cryptare/Decryptare: ");
+        while (!sc.hasNextInt()) {
+            System.out.println("Cheia trebuie să fie un număr!");
+            sc.next();
+        }
+        int key = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("\nAlgoritmul Cesar. Selectează una dintre opțiuni:");
+        System.out.println("1. Criptare");
+        System.out.println("2. Decriptare");
+
+        while (!sc.hasNextInt()) {
+            System.out.println("Introdu o opțiune validă (1 sau 2):");
+            sc.next();
+        }
+        int option = sc.nextInt();
+        sc.nextLine();
+
+        switch (option) {
+            case 1 -> {
+                System.out.print("Introdu textul care trebuie criptat: ");
+                String ch = sc.nextLine();
+                Encrypt.encrypt(ch, key);
+            }
+            case 2 -> {
+                System.out.print("Introdu textul care trebuie decriptat: ");
+                String ch = sc.nextLine();
+                Decrypt.decrypt(ch, key);
+            }
+            default -> {
+                System.out.println("Ați introdus o opțiune greșită!");
+                System.exit(404);
+            }
+        }
+
+        sc.close();
+    }
+}
