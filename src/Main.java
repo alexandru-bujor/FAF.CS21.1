1493de9e836bc4ee5b65e428c2abd1caca4b2021
Sandu Bujor <alexandru.bujor@isa.utm.md>
2025-10-08 16:19:49 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,5 @@
 
 
 void main() {
-      IO.println(String.format("Hello and welcome!"));
-
-    for (int i = 1; i <= 5; i++) {
-        IO.println("i = " + i);
-    }
+    System.out.println("Initial commit");
 }
