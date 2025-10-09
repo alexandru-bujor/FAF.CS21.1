ce877206db2ce41bebbf61ab282b98bf80e933ca
Sandu Bujor <alexandru.bujor@isa.utm.md>
2025-10-09 18:50:17 +0300

    un meniu simplu in consola in care se selecteaza ce sarcina sa fie rulata

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
-
-
+import task1.*;
+import java.util.*;
 
 void main() {
-    System.out.println("Initial commit");
+
+    Scanner sc = new Scanner(System.in);
+
+
+
+
+    System.out.println("Selecteaza unul dintre urmatoarele taskuri:\n");
+    System.out.println("1. Sarcina 1.1");
+    System.out.println("2. Sarcina 1.2");
+    int option = sc.nextInt();
+
+    switch (option) {
+        case 1 -> {
+
+        }
+        case 2 -> {
+
+        }
+        default -> {
+            System.out.println("Ați introdus o opțiune greșită!");
+            System.exit(404);
+        }
+    }
 }
